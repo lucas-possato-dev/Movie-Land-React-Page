4eda7ddce5b2e489328b640d84288ed62bdee45d
lucas-possato-dev <lucaspstinho@gmail.com>
2023-04-15 16:26:24 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Movie from './Movie';
 
-function App() {
+const API_URL = 'http://www.omdbapi.com?apikey=d074b1ca';
+
+const App = () => {
+  const [movies, setMovies] = React.useState([]);
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  } 
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      searchMovies(searchTerm);
+    }
+  }
+
+  React.useEffect(() => {
+    searchMovies('Batman');
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='app'>
+        <h1>MovieLand</h1>
+        <div className='search'>
+          <input type="text" placeholder='Search for Movies' value={searchTerm} onKeyDown={handleKeyDown} onChange={(event) => setSearchTerm(event.target.value)}/>
+          <img src={SearchIcon} alt="search" onClick= {() => searchMovies(searchTerm)} />
+        </div>
+        {movies?.length > 0 ? (
+          <div className='container'>
+            {movies.map((movie) => (
+              <Link to={`/movie/${movie.imdbID}`} key={movie.imdbID}>
+                <MovieCard movie={movie} />
+              </Link>
+            ))}
+            <Routes>
+              <Route path='/movie/:id' element={<Movie />} />
+            </Routes>
+          </div>
+        ) : (
+          <div className='empty'>
+            <h2>No movies found</h2>
+          </div>
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Movie.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './Movie.module.css';
+
+const API_URL = 'http://www.omdbapi.com?apikey=d074b1ca';
+
+const Movie = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = React.useState(null);
+
+  React.useEffect(() => {
+    const fetchMovie = async () => {
+      const response = await fetch(`${API_URL}&i=${id}`);
+      const data = await response.json();
+      setMovie(data);
+    };
+    fetchMovie();
+  }, [id]);
+
+  const imgRef = React.useRef(null);
+
+  const updateImgPosition = () => {
+    if (imgRef.current) {
+      const windowHeight = window.innerHeight;
+      const imgHeight = imgRef.current.offsetHeight;
+      const top = Math.max((windowHeight - imgHeight) / 2, 0);
+      imgRef.current.style.top = `${top}px`;
+    }
+  };
+
+  updateImgPosition();
+
+  React.useEffect(() => {
+    updateImgPosition();
+    window.addEventListener('scroll', updateImgPosition);
+    return () => {
+      window.removeEventListener('scroll', updateImgPosition);
+    };
+  }, []);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div ref={imgRef} className={`${styles.movieDetails} ${styles.fontRaleway}`}>
+      <h2>{movie.Title}</h2>
+      <div>
+        <img src={movie.Poster} alt={movie.Title} />
+      </div>
+      <p>Year: {movie.Year}</p>
+      <p>Genre: {movie.Genre}</p>
+      <p>Director: {movie.Director}</p>
+      <p>Plot: {movie.Plot}</p>
+    </div>
+  );
+};
+
+export default Movie;
